20b968ff2c89d730f4ccf8dd03301cbc46ee291f
Ryotaro.Ieda <ryotaro@RyotaroIedanoMacBook-Air.local>
2022-09-02 17:03:58 +0900

    question

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get "/" => "home#top"
+  devise_for :users
+  resources :users
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
